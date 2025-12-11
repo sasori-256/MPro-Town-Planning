edb046caf3103f5e7e9932b7d51d3447500f1769
wowazun <ouskvulpes611@gmail.com>
2025-12-11 18:45:35 +0900

    chor: 一旦

---
+++ app/src/main/java/io/github/sasori_256/town_planning/controller/PlaceBuildingController.java
@@ -0,0 +1,11 @@
+package io.github.sasori_256.town_planning.controller;
+
+import java.awt.event.MouseListener;
+import.java.awt.geom.Point2D;
+
+
+public class PlaceBuildingController implements MouseListener{
+    private MapContext mapContext;
+
+    
+}

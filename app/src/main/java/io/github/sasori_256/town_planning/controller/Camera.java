53c6e11880f2afec289eb1050cd1a09d1d7244ff
wowazun <ouskvulpes611@gmail.com>
2025-12-12 00:39:39 +0900

    feat: Mapクリック時の動作を受け取れるようになった

--- app/src/main/java/io/github/sasori_256/town_planning/controller/Camera.java
+++ app/src/main/java/io/github/sasori_256/town_planning/controller/Camera.java
@@ -2,6 +2,7 @@ package io.github.sasori_256.town_planning.controller;
 import java.awt.geom.Point2D;
 import java.awt.Point;
 
+
 public class Camera {
     private int cellWidth;
     private int cellHeight;
@@ -40,13 +41,11 @@ public class Camera {
      * スクリーン座標をアイソメトリック座標に変換する
      * @param screenX スクリーンのX座標
      * @param screenY スクリーンのY座標
-     * @param offsetX X座標のオフセット
-     * @param offsetY Y座標のオフセット
      * @return アイソメトリック座標
      */
-    public Point2D.Double screenToIso(int screenX, int screenY, int offsetX, int offsetY){
-        double adjX = screenX - offsetX;
-        double adjY = screenY - offsetY;
+    public Point2D.Double screenToIso(int screenX, int screenY){
+        double adjX = screenX - this.offsetX;
+        double adjY = screenY - this.offsetY;
         double isoX = adjX / this.cellWidth + adjY / this.cellHeight;
         double isoY = adjY / this.cellHeight - adjX / this.cellWidth;
         return new Point2D.Double(isoX, isoY);
@@ -56,13 +55,11 @@ public class Camera {
      * アイソメトリック座標をスクリーン座標に変換する
      * @param isoX アイソメトリックのX座標
      * @param isoY アイソメトリックのY座標
-     * @param offsetX X座標のオフセット
-     * @param offsetY Y座標のオフセット
      * @return スクリーン座標
      */
-    public Point isoToScreen(double isoX, double isoY, int offsetX, int offsetY){
-        double screenX = (isoX - isoY) * (this.cellWidth / 2.0) + offsetX;
-        double screenY = (isoX + isoY) * (this.cellHeight / 2.0) + offsetY;
+    public Point isoToScreen(double isoX, double isoY){
+        double screenX = (isoX - isoY) * (this.cellWidth / 2.0) + this.offsetX;
+        double screenY = (isoX + isoY) * (this.cellHeight / 2.0) + this.offsetY;
         return new Point((int)screenX, (int)screenY);
     }
 
---
+++ app/src/main/java/io/github/sasori_256/town_planning/controller/moc/BuildingGameObject.java
@@ -0,0 +1,5 @@
+package io.github.sasori_256.town_planning.controller.moc;
+
+public class BuildingGameObject {
+
+}

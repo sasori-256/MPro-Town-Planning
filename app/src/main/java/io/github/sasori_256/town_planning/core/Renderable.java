13df12f54e86332173a119130c1211f3d0e36dcf
teruyoshii <teruki.koizumi@gmail.com>
2025-12-12 13:49:01 +0900

    関係ないファイルを削除

--- app/src/main/java/io/github/sasori_256/town_planning/core/Renderable.java
+++
@@ -1,9 +0,0 @@
-package io.github.sasori_256.town_planning.core;
-
-/**
- * 描画可能なオブジェクトを表すインターフェース。
- * 各オブジェクトは表示用のシンボルを持つ(予定)。
- */
-public interface Renderable {
-  String getDisplaySymbol();
-}
--- app/src/main/java/io/github/sasori_256/town_planning/event/EventType.java
+++
@@ -1,7 +0,0 @@
-package io.github.sasori_256.town_planning.event;
-
-public enum EventType {
-  MAP_UPDATED,
-  SOUL_CHANGED,
-  GAME_TICK
-}

30a82687d3a1ca5ed0ad15cfe6b12d0d6338b48e
黄泉比良坂46(むつみん) <72808408+sasori-256@users.noreply.github.com>
2025-12-12 13:19:35 +0900

    Delete app/src/main/java/io/github/sasori_256/town_planning/controller/moc/MapContext.java

--- app/src/main/java/io/github/sasori_256/town_planning/controller/moc/MapContext.java
+++
@@ -1,9 +0,0 @@
-package io.github.sasori_256.town_planning.controller.moc;
-
-import java.awt.geom.Point2D;
-
-public interface MapContext {
-
-    void placeBuilding(Point2D.Double isoPoint, BuildingGameObject selectedBuilding);
-
-}

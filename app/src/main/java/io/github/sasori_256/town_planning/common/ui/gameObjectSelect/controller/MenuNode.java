cb56d92fd6923d7c806f6c35108756910fc98e25
wowazun <ouskvulpes611@gmail.com>
2025-12-29 13:42:36 +0900

    fix: 55

--- app/src/main/java/io/github/sasori_256/town_planning/common/ui/gameObjectSelect/controller/MenuNode.java
+++ app/src/main/java/io/github/sasori_256/town_planning/common/ui/gameObjectSelect/controller/MenuNode.java
@@ -3,6 +3,10 @@ package io.github.sasori_256.town_planning.common.ui.gameObjectSelect.controller
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+/**
+ * メニューのノードを表すインターフェース.
+ * @implNote MenuNodeはActionListenerを継承しており、メニュー項目が選択されたときの動作を定義できる.
+ */
 public interface MenuNode extends ActionListener {
     String getName();
     boolean isLeaf();

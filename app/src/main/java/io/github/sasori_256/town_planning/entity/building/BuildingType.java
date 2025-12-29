2a48d7323a08581ccd1b512b2a884b475f6bc7de
teruyoshii <teruki.koizumi@gmail.com>
2025-12-29 16:01:38 +0900

    fix: buildingが無い、アスににerror_buildingを表示するのではなく、何も表示しないようにした

--- app/src/main/java/io/github/sasori_256/town_planning/entity/building/BuildingType.java
+++ app/src/main/java/io/github/sasori_256/town_planning/entity/building/BuildingType.java
@@ -2,9 +2,9 @@ package io.github.sasori_256.town_planning.entity.building;
 
 import java.util.function.Supplier;
 
-import io.github.sasori_256.town_planning.entity.model.GameEffect;
 import io.github.sasori_256.town_planning.entity.building.strategy.PopulationGrowthEffect;
 import io.github.sasori_256.town_planning.entity.model.CategoryType;
+import io.github.sasori_256.town_planning.entity.model.GameEffect;
 
 /**
  * 建物の種類定義。
@@ -14,7 +14,7 @@ public enum BuildingType {
   HOUSE("住居", "error_building", 50, 4, 100, () -> new PopulationGrowthEffect(4), CategoryType.RESIDENTIAL),
   CHURCH("教会", "error_building", 150, 0, 150, CategoryType.RELIGIOUS),
   GRAVEYARD("墓地", "error_building", 100, 0, 100, CategoryType.CEMETERY),
-  NONE("none", "error_building", 0, 0, 0, CategoryType.NONE);
+  NONE("none", "none", 0, 0, 0, CategoryType.NONE);
 
   private final String displayName;
   private final String imageName;

35481dc95e00f9f3ef6beec3bbd8c3c7a803975b
wowazun <ouskvulpes611@gmail.com>
2026-01-03 01:05:11 +0900

    fix: 直した

--- app/src/main/java/io/github/sasori_256/town_planning/App.java
+++ app/src/main/java/io/github/sasori_256/town_planning/App.java
@@ -1,16 +1,11 @@
 package io.github.sasori_256.town_planning;
 
-import java.awt.geom.Point2D;
-
 import io.github.sasori_256.town_planning.common.event.EventBus;
 import io.github.sasori_256.town_planning.common.ui.GameWindow;
 import io.github.sasori_256.town_planning.entity.Camera;
-import io.github.sasori_256.town_planning.entity.building.Building;
-import io.github.sasori_256.town_planning.entity.building.BuildingType;
 import io.github.sasori_256.town_planning.entity.model.GameModel;
 import io.github.sasori_256.town_planning.map.controller.GameMapController;
-import io.github.sasori_256.town_planning.map.controller.handler.PlaceBuildingHandler;
-import io.github.sasori_256.town_planning.map.model.GameMap; //いったん動かすために記載。Entity選択メニューが実装されたら消す。
+import io.github.sasori_256.town_planning.map.model.GameMap;
 
 public class App {
   public static void main(String[] args) {
@@ -22,9 +17,6 @@ public class App {
     GameMap gameMap = gameModel.getGameMap();
     Camera camera = new Camera(1, WIDTH, HEIGHT, eventBus);
     GameMapController gameMapController = new GameMapController(camera);
-    gameMapController.setActionOnClick(new PlaceBuildingHandler(gameMap, gameMapController)); // いったん動かすために記載。Entity選択メニューが実装されたら消す。
-    gameMapController.setSelectedEntityGenerator((point) -> new Building(point, BuildingType.HOUSE)); // いったん動かすために記載。Entity選択メニューが実装されたら消す。
-    GameWindow gameWindow = new GameWindow(gameMapController, gameMap, camera, WIDTH, HEIGHT, eventBus,
-        gameMapController);
+    GameWindow gameWindow = new GameWindow(gameMapController, gameMap, camera, WIDTH, HEIGHT, eventBus, gameMapController);
   }
 }

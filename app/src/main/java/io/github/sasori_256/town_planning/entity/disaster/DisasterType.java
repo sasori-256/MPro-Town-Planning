85adab2fb2e9e3c873bcdaac0cf8537073aab9e2
sasori-256 <Sasori.0274@gmail.com>
2026-01-12 00:47:50 +0900

    feat(Disaster): Enable to occurrence of meteor disaster(loading images)

--- app/src/main/java/io/github/sasori_256/town_planning/entity/disaster/DisasterType.java
+++ app/src/main/java/io/github/sasori_256/town_planning/entity/disaster/DisasterType.java
@@ -11,7 +11,7 @@ import io.github.sasori_256.town_planning.entity.model.GameAction;
  * 天災の種類定義。
  */
 public enum DisasterType {
-  METEOR("隕石", "meteor", 200, 3, 100, CategoryType.METEOR, (pos, type) -> new MeteorDisasterAction(pos, type)),
+  METEOR("隕石", "loading", 200, 3, 100, CategoryType.METEOR, (pos, type) -> new MeteorDisasterAction(pos, type)),
   PLAGUE("疫病", "plague", 150, 5, 20, CategoryType.PLAGUE);
 
   private final String displayName;
